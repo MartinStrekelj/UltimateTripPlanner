3179d4fc30b6c19b6d85a148eb170e65f97ae576
Martin Strekelj <martin.strekelj123@gmail.com>
2020-04-01 14:13:49 +0200

    Added maps and budget functionalities

--- script.js
+++ script.js
@@ -1,27 +1,99 @@
 // Map
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
+function initMap() {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(document.getElementById('googleMap'), {
+    zoom: 8,
+    // 46.066096,14.4620597 => Ljubljana
+    center: {lat: 46.066096, lng: 14.4620597}
+  });
+directionsRenderer.setMap(map);
 
-    let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var onClickHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+    getDistance();
+  };
+
+  document.getElementById('btn-submitLocation').addEventListener('click', onClickHandler);
+}
+
+calculateAndDisplayRoute = (directionsService, directionsRenderer) => {
+  const start = document.getElementById('location-input').value
+  const end   = document.getElementById('destination-input').value
+  directionsService.route(
+      {
+        origin: {query: start},
+        destination: {query: end},
+        travelMode: 'DRIVING'
+      },
+      function(response, status) {
+        if (status === 'OK') {
+          directionsRenderer.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+}
+
+getDistance = () => {
+  const origin = document.getElementById('location-input').value
+  const destination = document.getElementById('destination-input').value
+  const service = new google.maps.DistanceMatrixService()
+  service.getDistanceMatrix({
+    origins: [origin],
+    destinations: [destination],
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: false,
+    avoidTolls: false,
+  }, function(response,status){
+    if (status == 'OK'){
+      const distance = response.rows[0].elements[0].distance.text
+      document.getElementById("distance-label").innerText = "Gas (distance = " + distance + ")";
+      calculateBudget();
+    } else{
+      alert ("Error was: " + status);
     }
+  })
+}
 
 // Budget
 
-// onChange => {
-calculateBudget = () =>{
-  const budget    = getElementById("budget").value
-  const gas_price = getElementById("gas-price").value
-  const car_con   = getElementById("car-consumption").value
-  const living    = getElementById("living").value
-  const other     = getElementById("other-costs").value
-  // const distance => get from maps API
-  let Calculated_Gas = ((distance * (car_con / 100)) * gas_price)
-  let Calculated_Budget = budget - Calculated_Gas - living - other
-  document.getElementById("output").value = Calculated_Budget; 
+convertToNumber = (str) => {
+  if (str == ""){
+    return 0;
+  } else{
+    return Number(str);
+  }
 }
+
+calculateBudget = () =>{
+  const budget = document.getElementById("budget").value
+  if (budget != ""){
+    const distance_label = document.getElementById("distance-label").innerText;
+    let distance = 0;
+    if (distance_label.length  > 3){
+      let distance_str = (distance_label.substr(16, distance_label.length-1))
+      distance = Number((distance_str.replace(" km)", "")));
+    }
+    const gas_price = convertToNumber(document.getElementById("gas-price").value)
+    const car_con   = convertToNumber(document.getElementById("car-consumption").value)
+    const living    = convertToNumber(document.getElementById("living").value)
+    const other     = convertToNumber(document.getElementById("other-costs").value)
+    let Calculated_Gas = Math.round((distance * (car_con / 100)) * gas_price) * 2
+    let Calculated_Budget = budget - Calculated_Gas - living - other
+    let outputMSG
+    if (Calculated_Gas != 0){
+     outputMSG = `Total budget ${budget}€ - gas costs (${distance} * 2 = ${distance*2}km) ${Calculated_Gas}€ - other expenses (${living}€ + ${other}€) = ${Calculated_Budget}€`
+    }else{
+      outputMSG = `Total budget ${budget}€ - other expenses (${living}€ + ${other}€) = ${Calculated_Budget}€`
+    }
+    console.log(outputMSG)
+    document.getElementById("output").innerText = outputMSG;
+  }
+} 
+
+document.getElementById("calculate-btn").addEventListener("click", calculateBudget);
 
 
 // Items
@@ -36,7 +108,7 @@ delete_item = (e) => {
   target.parentNode.removeChild(target)
 }
 
-clear_all = () =>{
+clear_all = () => {
   const item_list = document.getElementById("item-list")
   while (item_list.firstChild){
     item_list.removeChild(item_list.firstChild)
